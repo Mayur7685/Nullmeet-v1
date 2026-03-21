3cbbc9e73bf8f28481a9700ff4d2f697c72c3c09
Mayur7685 <mayuras7685@gmail.com>
2026-03-21 14:49:31 +0530

    fix: deployment issue

--- next.config.ts
+++ next.config.ts
@@ -1,22 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  webpack: (config, { isServer }) => {
-    if (!isServer) {
-      config.resolve.fallback = {
-        ...config.resolve.fallback,
-        fs: false,
-        path: false,
-        os: false,
-        crypto: false,
-      };
-    }
-    config.externals = [
-      ...(Array.isArray(config.externals) ? config.externals : []),
-      "pino-pretty",
-    ];
-    return config;
-  },
+  turbopack: {},
 };
 
 export default nextConfig;
